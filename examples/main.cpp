056ced69a708ab273716be1c522055ab935c0f56
BIMCoderLiang <bim.frankliang@foxmail.com>
2025-02-08 22:00:41 +0800

    update:main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -8,5 +8,31 @@
  *
  */
 
+#include "ND_CurveEvalModule.h"
+#include <vector>
 
+void CurveFitting()
+{
+	int num_ctrl_pts = 32;
+	int degree = 3;
+	int num_eval_pts = 128;
 
+	ND_LNLib::CurveEvalModule cm(num_ctrl_pts, degree, num_eval_pts);
+
+	
+	std::vector<torch::optim::OptimizerParamGroup> group;
+	torch::optim::AdamOptions options(0.1);
+	torch::optim::Adam opt(group, options);
+	torch::optim::ReduceLROnPlateauScheduler rp();
+
+	for (int i = 0; i < 100000; i++)
+	{
+		opt.zero_grad();
+
+	}
+}
+
+int main(int, char* [])
+{
+	
+}
