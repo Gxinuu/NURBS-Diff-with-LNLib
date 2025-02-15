1b0b8c2a054d7590fdbc320359f3e1c4bd336b67
Gxinuu <2724878814@qq.com>
2025-02-16 01:42:41 +0800

    support vscode compile

--- build.bat
+++ build.bat
@@ -1,8 +1,19 @@
 @echo off
 set oldPath=%cd%
 
-cd /d %~dp0
-cmake -S . -B build
-cmake --open build
+
+:: 设置 MSVC 编译器环境变量（路径根据你的安装位置调整）
+call "D:\VisiualStudio\visualstudio\VC\Auxiliary\Build\vcvars64.bat"
+:: 设置项目路径
+set PROJECT_ROOT=%~dp0
+cd /d "%PROJECT_ROOT%"
+
+:: 清理旧的构建目录
+if exist build rmdir /s /q build
+
+
+@REM cd /d %~dp0
+cmake -S . -B build -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake --build build
 
 cd  /d %oldPath%
