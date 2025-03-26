7c7f01e6ceaa83c68e68a462649b8dbcf35315b5
BIMCoderLiang <bim.frankliang@foxmail.com>
2025-03-26 18:05:30 +0800

    update simple surface sample.

--- examples/main.cpp
+++ examples/main.cpp
@@ -164,53 +164,72 @@ void CurveFitting(vtkSmartPointer<vtkRenderer> renderer)
 
 void SurfaceFitting(vtkSmartPointer<vtkRenderer> renderer)
 {
-    int degreeU = 3;
-    int degreeV = 3;
-    std::vector<double> kvU = { 0,0,0,0,0.4,0.6,1,1,1,1 };
-    std::vector<double> kvV = { 0,0,0,0,0.4,0.6,1,1,1,1 };
-    std::vector<std::vector<XYZW>> controlPoints(6, std::vector<XYZW>(6));
+    int degreeU = 2;
+    int degreeV = 2;
+    std::vector<double> kvU = { 0,0,0,1,2,3,4,4,5,5,5 };
+    std::vector<double> kvV = { 0,0,0,1,2,3,3,3 };
+    UV uv = UV(5.0 / 2, 1);
 
-    controlPoints[0][0] = XYZW(0, 0, 0, 1);
-    controlPoints[0][1] = XYZW(6.666666, 0, 4, 1);
-    controlPoints[0][2] = XYZW(16.666666, 0, 22, 1);
-    controlPoints[0][3] = XYZW(33.333333, 0, 22, 1);
-    controlPoints[0][4] = XYZW(43.333333, 0, 4, 1);
-    controlPoints[0][5] = XYZW(50, 0, 0, 1);
+    XYZW P20 = XYZW(-1, 2, 4, 1);
+    XYZW P21 = XYZW(0, 2, 4, 1);
+    XYZW P22 = XYZW(0, 6, 4, 2);
+    XYZW P23 = XYZW(0, 2, 0, 1);
+    XYZW P24 = XYZW(1, 2, 0, 1);
 
-    controlPoints[1][0] = XYZW(0, 6.666667, 0, 1);
-    controlPoints[1][1] = XYZW(6.6666667, 6.666667, 9.950068, 1);
-    controlPoints[1][2] = XYZW(16.6666666, 6.666667, 9.65541838, 1);
-    controlPoints[1][3] = XYZW(33.3333333, 6.666667, 47.21371742, 1);
-    controlPoints[1][4] = XYZW(43.3333333, 6.666667, -11.56982167, 1);
-    controlPoints[1][5] = XYZW(50, 6.6666667, 0, 1);
+    XYZW P10 = 0.9 * P20;
+    XYZW P11 = 0.9 * P21;
+    XYZW P12 = 0.9 * P22;
+    XYZW P13 = 0.9 * P23;
+    XYZW P14 = 0.9 * P24;
 
-    controlPoints[2][0] = XYZW(0, 16.666666, 0, 1);
-    controlPoints[2][1] = XYZW(6.6666667, 16.666666, -7.9001371, 1);
-    controlPoints[2][2] = XYZW(16.6666666, 16.666666, 46.6891632, 1);
-    controlPoints[2][3] = XYZW(33.3333333, 16.666667, -28.4274348, 1);
-    controlPoints[2][4] = XYZW(43.3333333, 16.666667, 35.1396433, 1);
-    controlPoints[2][5] = XYZW(50, 16.6666667, 0, 1);
+    XYZW P00 = 0.9 * P10;
+    XYZW P01 = 0.9 * P11;
+    XYZW P02 = 0.9 * P12;
+    XYZW P03 = 0.9 * P13;
+    XYZW P04 = 0.9 * P14;
 
-    controlPoints[3][0] = XYZW(0, 33.3333333, 0, 1);
-    controlPoints[3][1] = XYZW(6.6666667, 33.3333333, 29.2877911, 1);
-    controlPoints[3][2] = XYZW(16.6666666, 33.3333333, -30.4644718, 1);
-    controlPoints[3][3] = XYZW(33.3333333, 33.3333333, 129.1582990, 1);
-    controlPoints[3][4] = XYZW(43.3333333, 33.3333333, -62.1717142, 1);
-    controlPoints[3][5] = XYZW(50, 33.333333, 0, 1);
+    XYZW P30 = XYZW(3, 6, 8, 2);
+    XYZW P31 = XYZW(4, 6, 8, 2);
+    XYZW P32 = XYZW(12, 24, 12, 6);
+    XYZW P33 = XYZW(4, 6, 0, 2);
+    XYZW P34 = XYZW(5, 6, 0, 2);
 
-    controlPoints[4][0] = XYZW(0, 43.333333, 0, 1);
-    controlPoints[4][1] = XYZW(6.6666667, 43.333333, -10.384636, 1);
-    controlPoints[4][2] = XYZW(16.6666666, 43.333333, 59.21371742, 1);
-    controlPoints[4][3] = XYZW(33.3333333, 43.333333, -37.7272976, 1);
-    controlPoints[4][4] = XYZW(43.3333333, 43.333333, 45.1599451, 1);
-    controlPoints[4][5] = XYZW(50, 43.333333, 0, 1);
+    XYZW P40 = XYZW(3, 2, 4, 1);
+    XYZW P41 = XYZW(4, 2, 4, 1);
+    XYZW P42 = XYZW(8, 6, 4, 2);
+    XYZW P43 = XYZW(4, 2, 0, 1);
+    XYZW P44 = XYZW(5, 2, 0, 1);
 
-    controlPoints[5][0] = XYZW(0, 50, 0, 1);
-    controlPoints[5][1] = XYZW(6.6666667, 50, 0, 1);
-    controlPoints[5][2] = XYZW(16.6666666, 50, 0, 1);
-    controlPoints[5][3] = XYZW(33.3333333, 50, 0, 1);
-    controlPoints[5][4] = XYZW(43.3333333, 50, 0, 1);
-    controlPoints[5][5] = XYZW(50, 50, 0, 1);
+    XYZW P50 = 1.5 * P40;
+    XYZW P51 = 1.5 * P41;
+    XYZW P52 = 1.5 * P42;
+    XYZW P53 = 1.5 * P43;
+    XYZW P54 = 1.5 * P44;
+
+    XYZW P60 = 1.5 * P50;
+    XYZW P61 = 1.5 * P51;
+    XYZW P62 = 1.5 * P52;
+    XYZW P63 = 1.5 * P53;
+    XYZW P64 = 1.5 * P54;
+
+    XYZW P70 = 1.5 * P60;
+    XYZW P71 = 1.5 * P61;
+    XYZW P72 = 1.5 * P62;
+    XYZW P73 = 1.5 * P63;
+    XYZW P74 = 1.5 * P64;
+
+    std::vector<std::vector<XYZW>> cps = {
+
+        {P00, P01, P02, P03, P04},
+        {P10, P11, P12, P13, P14},
+        {P20, P21, P22, P23, P24},
+        {P30, P31, P32, P33, P34},
+        {P40, P41, P42, P43, P44},
+        {P50, P51, P52, P53, P54},
+        {P60, P61, P62, P63, P64},
+        {P70, P71, P72, P73, P74},
+
+    };
 
     //Target Surface
     LN_NurbsSurface surface;
@@ -218,7 +237,7 @@ void SurfaceFitting(vtkSmartPointer<vtkRenderer> renderer)
     surface.DegreeV = degreeV;
     surface.KnotVectorU = kvU;
     surface.KnotVectorV = kvV;
-    surface.ControlPoints = controlPoints;
+    surface.ControlPoints = cps;
     DisplaySurface(renderer, surface, true);
 
     //Start Fitting
@@ -226,14 +245,14 @@ void SurfaceFitting(vtkSmartPointer<vtkRenderer> renderer)
     int num_ctrl_pts_v = 12;
     int num_eval_pts_u = 64;
     int num_eval_pts_v = 64;
-    double step_u = (kvU.back() - kvU[0]) / (num_eval_pts_u - 1);
-    double step_v = (kvV.back() - kvV[0]) / (num_eval_pts_v - 1);
+    double step_u = (kvU.back() - kvU.front()) / (num_eval_pts_u - 1);
+    double step_v = (kvV.back() - kvV.front()) / (num_eval_pts_v - 1);
 
     std::vector<XYZ> targetPoints;
     for (int i = 0; i < num_eval_pts_u; ++i) {
-        double u = kvU[0] + i * step_u;
+        double u = kvU.front() + i * step_u;
         for (int j = 0; j < num_eval_pts_v; ++j) {
-            double v = kvV[0] + j * step_v;
+            double v = kvV.front() + j * step_v;
             targetPoints.push_back(NurbsSurface::GetPointOnSurface(surface, UV(u, v)));
         }
     }
@@ -247,8 +266,7 @@ void SurfaceFitting(vtkSmartPointer<vtkRenderer> renderer)
     }
 
     // Initialize trainable control points [1, 12, 12, 3]
-    auto inp_ctrl_pts = torch::randn({1, num_ctrl_pts_u, num_ctrl_pts_v, 3},
-        torch::dtype(torch::kFloat).requires_grad(true));
+    auto inp_ctrl_pts = torch::randn({1, num_ctrl_pts_u, num_ctrl_pts_v, 3}, torch::dtype(torch::kDouble).requires_grad(true));
 
     auto eval_module = std::make_shared<ND_LNLib::SurfaceEvalModule>(
         num_ctrl_pts_u, num_ctrl_pts_v,
